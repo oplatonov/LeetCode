ff24ca6ab5195e1e25cc60b83c349df858faf8bd
Oleg Platonov <platonovrf@bk.ru>
2021-08-04 22:24:15 +0300

    Добавьте файлы проекта.

---
+++ LeetCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using TwoSum;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intArray = new[] { 2, 7, 11, 15 };
+            int target = 9;
+
+            var result = Solution.TwoSum(intArray, target);
+        }
+    }
+}
---
+++ LeetCode/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace TwoSum
+{
+    public class Solution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var hashTable = new Hashtable();
+            for (int i = 0; i < nums.Count(); i++)
+            {
+                if (!hashTable.ContainsKey(nums[i]))
+                    hashTable.Add(nums[i], i);
+
+                int remaining = target - nums[i];
+                if (hashTable.ContainsKey(remaining) 
+                    && (int)hashTable[remaining] != i)
+                    return new int[] { (int)hashTable[remaining], i };
+            }
+
+            throw new ArgumentException("Two sum not found");
+        }
+    }
+}
